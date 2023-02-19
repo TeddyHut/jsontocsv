1a76d08ca9bf799bbebf9799001902bcc305adcf
TeddyHut <teddy.hutteddy@gmail.com>
2023-02-20 09:27:05 +1100

    Implement baseline functionality
    Writes JSON at URL to CSV file as intended.

--- jsontocsv.py
+++ jsontocsv.py
@@ -1,12 +1,41 @@
 import argparse
+import requests
+import datetime
+import time
 
 if __name__ == "__main__":
     DEFAULT_OUTPUT_FILENAME = "output.csv"
-    parser = argparse.ArgumentParser(description="Convert json at http URL to CSV at intervals", prog="jsontocsv")
+    parser = argparse.ArgumentParser(description="Convert an array of json objects at a http URL to CSV at regular intervals", prog="jsontocsv")
     parser.add_argument("-s", "--silent", action="store_true", help="only print output on error")
     parser.add_argument("-v", "--verbose", action="store_true", help="print on every request")
-    parser.add_argument("-t", "--time-interval", metavar="time", type=float, help="polling interval in seconds")
+    parser.add_argument("-d", "--delay", help="delay between requests in seconds [default: 10]", default=10)
     parser.add_argument("url", help="URL of json")
     parser.add_argument("output", help=f"CSV output file name [default: {DEFAULT_OUTPUT_FILENAME}]", nargs="?", default=DEFAULT_OUTPUT_FILENAME)
 
     args = parser.parse_args()
+
+    previous_hash = None
+    while True:
+        reqtime_str = datetime.datetime.now().strftime("%H:%M:%S.%f")[:-3]
+        if args.verbose:
+            print(f"Request at: {reqtime_str}")
+
+        r = requests.get(args.url)
+        r.raise_for_status()
+
+        current_hash = hash(r.text)
+        if current_hash != previous_hash:
+            previous_hash = current_hash
+            if not args.silent:
+                print(f"Update  at: {reqtime_str}")
+
+            jsondata = r.json()
+
+            with open(args.output, "w") as csvfile:
+                # Write header row - assumes that jsondata[0] exists
+                csvfile.write(",".join(f"\"{x}\"" for x in jsondata[0].keys()) + "\n")
+                # Write data rows
+                for row in jsondata:
+                    csvfile.write(",".join(f"\"{x}\"" for x in row.values()) + "\n")
+
+        time.sleep(args.delay)
